8b52511bc5564512d5ffcb8d2e42bb370d06167e
Helio Alves <hellius.af@gmail.com>
2022-09-18 09:31:00 -0300

    Projeto restaurante saboroso

---
+++ inc/reservations.js
@@ -0,0 +1,112 @@
+var pagination = require("./../inc/pagination")
+var conn = require("./db")
+
+module.exports = {
+
+    render(req, res, error, success) {
+
+        res.render('reservations',{
+            title: 'Reservas - Restaurante Saboroso',
+            background: 'images/img_bg_2.jpg',
+            h1: 'Reserve uma Mesa!',
+            body:req.body,
+            error,
+            success
+          });
+
+    },
+
+    save(fields){        
+
+        return new Promise((resolve, reject) => {
+
+            if (fields.date.indexOf('/') > -1) {
+
+                let date = fields.date.split('/')        
+                fields.date =  `${date[2]}-${date[1]}-${date[0]}`
+
+            }
+
+       
+            let query, params = [
+                fields.name,
+                fields.email,
+                fields.people,
+                fields.date,
+                fields.time
+            ];
+
+            if(parseInt(fields.id) > 0 ){
+
+                query = `
+                    UPDATE tb_reservations
+                    SET
+                        name = ?,
+                        email = ?,
+                        people = ?,
+                        date = ?,
+                        time = ?
+                    WHERE   ID = ?                
+                `;
+
+                params.push(fields.id);
+
+            }else{
+
+                query = `
+                    INSERT INTO tb_reservations (name, email, people, date, time)
+                    VALUES(?, ?, ?, ?, ?)                
+                `;
+
+            }
+
+            conn.query(query, params, (err, results) =>{
+                
+                if(err) {
+                    reject(err)
+                } else{
+                    resolve(results);
+                }
+
+            })
+
+        })
+
+    },
+    getReservations(page) {
+
+        if (!page) page = 1
+
+        let pag = new pagination(
+
+            `
+                SELECT SQL_CALC_FOUND_ROWS * FROM tb_reservations ORDER BY name LIMIT ?, ?
+            `
+        )
+        
+        return pag.getPage(page)
+    },
+    delete(id){
+
+        return new Promise((resolve, reject) => {
+
+            conn.query(`            
+                DELETE FROM tb_reservations WHERE id = ?            
+            `, [
+                id
+            ],(err, results) => {
+
+                if(err) {
+                    reject(err); 
+                } else{
+                    resolve(results)
+                }
+
+            })
+
+
+        })
+
+    }
+
+}
---
+++ public/js/hcode-formsave.js
@@ -0,0 +1,39 @@
+HTMLFormElement.prototype.save = function(config) {
+
+    let form = this;
+
+    form.addEventListener('submit',(e)=>{
+
+        e.preventDefault();
+
+        let formData = new FormData(form);
+     
+        fetch(form.action,{
+          method: form.method,
+          body:formData
+        })
+        .then(response => response.json())
+        .then( json => {
+
+            if (json.error){
+                
+                if(typeof config.failure === 'function') config.failure(json.error)
+
+            }else{
+                if(typeof config.success === 'function') config.success(json)
+
+            }
+
+            
+            //   console.log("JSON", json)     
+          // window.location.reload();
+        }).catch(err => {
+            if(typeof config.failure === 'function') config.failure(err)
+
+        })
+     
+    });
+
+    
+
+}
